7e45b5e701a6ba415a2e265a83a6237f1c36d0e7
Bijay Aashish Bhatta <34814966+psyphernix@users.noreply.github.com>
2021-12-18 03:36:20 +0545

    Update deployReactS3.sh

--- CID/deployReactS3.sh
+++ CID/deployReactS3.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-dir="react/"
+
+cd "${0%/*}"
+dir="./react"
 bucket="bab-react-s3-ga"
+
 echo "Deploying react app to s3 bucket $bucket"
 aws s3api create-bucket --bucket $bucket --region us-east-2 --create-bucket-configuration LocationConstraint=us-east-2
 aws s3 cp --recursive $dir s3://$bucket/
-aws s3api put-bucket-policy --bucket $bucket --policy "`cat bucpolicy.json`"
+aws s3api put-bucket-policy --bucket $bucket --policy '{"Version":"2012-10-17","Statement":[{"Sid": "AddPerm","Effect": "Allow","Principal": "*","Action": "s3:GetObject","Resource": "arn:aws:s3:::'$bucket'/*"}]}'
 aws s3 website s3://$bucket --index-document index.html --error-document index.html
 echo "Successfully deployed to s3"
